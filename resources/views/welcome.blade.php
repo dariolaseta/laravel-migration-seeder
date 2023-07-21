45bb48ce40be9ec3b534e98c6ed60fee9b50ae8b
Oxidra <dantelezzo@gmail.com>
2023-07-21 14:33:02 +0200

    wrote on dom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,18 @@
 @section('title', 'Hello World')
 
 @section('main-content')
-    <h1> Hello world </h1>
+    @foreach($trains as $train)
+    <div class="card">
+
+        <h1>{{"Agency: " . $train["agency"]}}</h1>
+        <h2>{{"Departure station: " . $train["departure_station"]}}</h2>
+
+        <p>{{"Arrival station: " . $train["arrival_station"]}}</p>
+        <p>{{"Departure time: " . $train["arrival_time"]}}</p>
+        <p>{{"Number of carriages: " . $train["number_of_carriages"]}}</p>
+        <p>{{"Is in time: " . $train["in_time"]}}</p>
+        <p>{{"Is deleted: " . $train["deleted"]}}</p>
+
+    </div>
+    @endforeach
 @endsection
