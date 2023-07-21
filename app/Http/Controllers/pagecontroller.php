72d51afa1aa8fd06a24ca3d69535078098704c5a
Oxidra <dantelezzo@gmail.com>
2023-07-21 14:10:46 +0200

    added index function to pagecontroller

--- app/Http/Controllers/pagecontroller.php
+++ app/Http/Controllers/pagecontroller.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\train;
+
 use Illuminate\Http\Request;
 
 class pagecontroller extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::all();
+
+        return view("welcome", compact("trains"));
+    }
 }
